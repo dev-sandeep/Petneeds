132a80fad5498a8b38da026fed082793ef0485f1
sandeep <dev.sandeep@hotmail.com>
2020-02-11 19:15:37 +0530

    update in pic

---
+++ src/Components/Buy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Buy() {
+    return (
+        <div className="buy-section" id="buy">
+         
+         
+        </div>
+    );
+}
+
+export default Buy;
---
+++ src/Components/Sell.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sell() {
+    return (
+        <div className="sell-section" id="sell">
+            
+        </div>
+    );
+}
+
+export default Sell;
